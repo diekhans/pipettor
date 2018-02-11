fdf9896151272b0ced7504aadcb756d6e17734e5
Mark Diekhans <markd@soe.ucsc.edu>
2018-02-11 15:24:07 -0800

    Bump version: 0.1.1 → 0.2.0

--- src/pipettor/__init__.py
+++ src/pipettor/__init__.py
@@ -8,7 +8,7 @@ from pipettor.exceptions import PipettorException, ProcessException
 from pipettor.devices import Dev, DataReader, DataWriter, File
 from pipettor.processes import Pipeline, Popen, setDefaultLogger, getDefaultLogger, setDefaultLogLevel, getDefaultLogLevel, setDefaultLogging
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 
 def run(cmds, stdin=None, stdout=None, stderr=DataReader, logger=None, logLevel=None):
