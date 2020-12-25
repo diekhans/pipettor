6ede9a1e6dc82de7a269abbfe8b57ee8e6548394
Mark Diekhans <markd@ucsc.edu>
2020-12-25 00:07:50 -0800

    Switch to using subprocess as a base rather interface directly interface with Unix system calls.  This lets subprocess deal with various issues dealing with the Python interpreter environment.

--- setup.py
+++ setup.py
@@ -43,9 +43,9 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Operating System :: POSIX',
         'Operating System :: MacOS :: MacOS X',
         'Topic :: Software Development :: Libraries :: Python Modules'
